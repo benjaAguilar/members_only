62d1a04f3f230bc7686e18e432b7963345f0e9ba
Benja Aguilar <benjaaguilar02@gmail.com>
2024-09-04 14:44:51 -0300

    feat: add getSignUp route and getLogIn route

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,23 @@ function getIndex(req, res){
     res.render('index');
 }
 
+function getSignUp(req, res){
+    const context = {
+        username: '', 
+        firstname: '',
+        lastname: '',
+        password: ''
+    };
+
+    res.render('signUp', context);
+}
+
+function getLogIn(req, res){
+    res.render('logIn');
+}
+
 module.exports = {
-    getIndex
+    getIndex,
+    getSignUp,
+    getLogIn
 }
