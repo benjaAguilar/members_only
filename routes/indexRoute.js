aae8ccf988e18bdc7ea2e2bfa59a7850bc05eff4
Benja Aguilar <benjaaguilar02@gmail.com>
2024-09-04 14:51:36 -0300

    feat: add routes sing up, log in and post

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,7 +1,14 @@
 const { Router } = require('express');
 const indexController = require('../controllers/indexController');
+const userController = require('../controllers/userController');
 const router = Router();
 
 router.get('/', indexController.getIndex);
+
+router.get('/sign-up', indexController.getSignUp);
+
+router.get('/log-in', indexController.getLogIn);
+
+router.post('/sign-user', userController.postSingUser);
 
 module.exports = router;
