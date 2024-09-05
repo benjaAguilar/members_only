66d9b8d58de265c60b40df8763961fe82b51ab5c
Benja Aguilar <benjaaguilar02@gmail.com>
2024-09-05 13:58:54 -0300

    feat: add member routes ref: ref routes order

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -6,14 +6,22 @@ const router = Router();
 
 router.get('/', tryCatch(indexController.getIndex));
 
+//sing up
 router.get('/sign-up', tryCatch(indexController.getSignUp));
-
-router.get('/log-in', tryCatch(indexController.getLogIn));
 
 router.post('/sign-up', userController.postSingUser);
 
+//log-in 
+router.get('/log-in', tryCatch(indexController.getLogIn));
+
 router.post('/log-in', tryCatch(userController.postLogUser));
 
+//logout
 router.get('/log-out', tryCatch(userController.getLogOutUser));
+
+//member
+router.get('/member', tryCatch(indexController.getMember));
+
+router.post('/member', userController.postGiveMembership);
 
 module.exports = router;
