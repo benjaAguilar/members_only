68c42f5f5bed415a3fbdd923f7a9be25974a625c
Benja Aguilar <benjaaguilar02@gmail.com>
2024-09-05 11:22:19 -0300

    feat: handle login error

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -1,10 +1,15 @@
-const { customError } = require("../utils/customErrors");
+const { customError, loginError } = require("../utils/customErrors");
 
 async function errorHandler(error, req, res, next){
     
     console.log(error);
     console.log('------------------');
     console.log(error.message);
+
+    if(error instanceof loginError){
+        res.status(error.statusCode).render('logIn');
+        return;
+    }
 
     if(error instanceof customError){
         res.status(error.statusCode).render('index');
