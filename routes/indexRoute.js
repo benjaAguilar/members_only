82dbdde46a8183100721e609c4c86981a5f5dcd2
Benja Aguilar <benjaaguilar02@gmail.com>
2024-09-05 09:05:27 -0300

    feat: add tryCatch func

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
 const indexController = require('../controllers/indexController');
 const userController = require('../controllers/userController');
+const { tryCatch } = require('../utils/tryCatch');
 const router = Router();
 
-router.get('/', indexController.getIndex);
+router.get('/', tryCatch(indexController.getIndex));
 
-router.get('/sign-up', indexController.getSignUp);
+router.get('/sign-up', tryCatch(indexController.getSignUp));
 
-router.get('/log-in', indexController.getLogIn);
+router.get('/log-in', tryCatch(indexController.getLogIn));
 
-router.post('/sign-user', userController.postSingUser);
+router.post('/sign-up', userController.postSingUser);
 
 module.exports = router;
