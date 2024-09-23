c89d9685875edd4bed31774cf615d71a7eb34914
Benja Aguilar <benjaaguilar02@gmail.com>
2024-09-23 11:36:37 -0300

    feat: add delete, like and getComments controllers
    delete: deletes clicked message if you are an admin
    like: like a message if you are a member
    comments: get a message and his comments

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -32,8 +32,49 @@ const postCreateMessage = [
             res.redirect('/');
         }
     )
-]
+];
+
+const postDeleteMessage = async (req, res, next) => {
+    if(!req.user){
+        return next(new customError('You are not authenticated', 400));
+    }
+
+    if(!req.user.admin){
+        return next(new customError('You are not an admin', 400));
+    }
+
+    console.log(req.params.id);
+    await db.deleteMessage(req.params.id);
+    res.redirect('/');
+}
+
+const postLikeMessage = async (req, res, next) => {
+
+    if(!req.user){
+        return next(new customError('You are not authenticated', 400));
+    }
+
+    if(!req.user.member){
+        return next(new customError('You are not a member', 400));
+    }
+
+    await db.increaseLikes(req.params.id);
+    res.redirect('/');
+}
+
+const getComments = async (req, res, next) => {
+
+    const results = await Promise.all([db.getMessage(req.params.id), db.getMessageComments(req.params.id)]);
+    const message = results[0][0];
+    const comments = results[1];
+    console.log("🚀 ~ getComments ~ comments:", comments)
+    
+    res.render('comments', {msg: message, comments: comments});
+}
 
 module.exports = {
-    postCreateMessage
+    postCreateMessage,
+    postDeleteMessage,
+    postLikeMessage,
+    getComments
 }
